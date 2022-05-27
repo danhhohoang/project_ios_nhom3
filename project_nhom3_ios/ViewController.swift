23adca9699d034c218fb6689339d8817b2e0f10f
danhhohoang <hohoangdanh.tdc2020@gmail.com>
2022-05-27 17:07:55 +0700

    Initial Commit

---
+++ project_nhom3_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project_nhom3_ios
+//
+//  Created by CNTT on 5/27/22.
+//  Copyright © 2022 fit.tdc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ project_nhom3_iosTests/project_nhom3_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  project_nhom3_iosTests.swift
+//  project_nhom3_iosTests
+//
+//  Created by CNTT on 5/27/22.
+//  Copyright © 2022 fit.tdc. All rights reserved.
+//
+
+import XCTest
+@testable import project_nhom3_ios
+
+class project_nhom3_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
